ae75fe3a9433cbd67b29f3e27827a9acb531e8cd
rlebhar-ext <111140836+codiku-dev@users.noreply.github.com>
2026-06-28 23:02:21 +0200

    lune 1.0.0

--- Casks/lune.rb
+++ Casks/lune.rb
@@ -1,6 +1,6 @@
 cask "lune" do
   version "1.0.0"
-  sha256 "c4bec48ecfe4c0a44aa4e7b605ec41819a12aea80170c327736644673fbab7fe"
+  sha256 "852019226de71a2b0f89013cae21336ff9bbdec9c6a57f6865561b1ff82534b5"
 
   url "https://github.com/codiku-dev/lune-releases/releases/download/v#{version}/Lune-#{version}.dmg"
   name "Lune"
